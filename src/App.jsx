69ddf6358020101cd6a1a7c93f2170407d0c8f77
Rishabh Dora <rishabhd.1121@gmail.com>
2024-09-16 17:43:44 +0530

    Setting up App file

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,33 @@
- import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import { useDispatch } from 'react-redux'
 import authService from './appwrite/auth'
+import {login, logout} from './store/authSlice'
 
 function App() {
   const [loading, setLoading] = useState(true)
   const dispatch = useDispatch()
   
-  
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userdata) => {
+        if(userdata){
+          useDispatch(login({userdata}))
+        }
+        else{
+          useDispatch(logout())
+        }
+      })
+      .finally(setLoading(false))
+  }, [])
+    
 
-  return (
+  //conditional rendering
+  return !loading ? (
     <>
       <h1>Welcome to the Mega Blog App!</h1>
     </>
-  )
+  ) : (null) 
 }
 
 export default App
--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import authSlice from "./authSlice";
 
 // Define your reducers here
 const store = configureStore({
     reducer:{
-
+        auth: authSlice
+        //Todo: add more slices here for posts
     }
 });
 
